19c4c7d5da5e01a74daf2a6b93211aa1d59c3b5b
Akila Mihiranga <akilamihiranganew475@gmail.com>
2019-09-18 23:53:44 +0530

    Food Ordering App

---
+++ app/src/main/java/com/example/foodhut/ViewHolder/MenuViewHolder.java
@@ -0,0 +1,37 @@
+package com.example.foodhut.ViewHolder;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.foodhut.Interface.ItemClickListener;
+import com.example.foodhut.R;
+
+public class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtMenuName;
+    public ImageView imageView;
+
+    private ItemClickListener itemClickListener;
+
+    public MenuViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        txtMenuName = itemView.findViewById(R.id.menu_name);
+        imageView = itemView.findViewById(R.id.menu_image);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.onClick(view,getAdapterPosition(),false);
+    }
+}
