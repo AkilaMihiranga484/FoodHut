c1970d1b687bb0ed1545a866365e27b62dc4d50f
Ishan Peiris <53204674+Ishan-hub@users.noreply.github.com>
2019-09-23 18:36:12 +0530

    sign Up

--- app/src/main/java/com/example/foodhut/SignUp.java
+++ app/src/main/java/com/example/foodhut/SignUp.java
@@ -20,65 +20,10 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUp extends AppCompatActivity {
 
-    MaterialEditText Phone,Name,Password;
-    Button SignUp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-        Name = findViewById(R.id.cusName);
-        Password = findViewById(R.id.cusPassword);
-        Phone = findViewById(R.id.cusPhone);
-
-        SignUp = findViewById(R.id.btnSignUp);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference table_user = database.getReference("User");
-
-        SignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-               if(Common.isConnectedToInternet(getBaseContext())) {
-
-                   final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
-                   mDialog.setMessage("Please wait...");
-                   mDialog.show();
-
-                   table_user.addValueEventListener(new ValueEventListener() {
-                       @Override
-                       public void onDataChange(DataSnapshot dataSnapshot) {
-
-                           //Check if user phone number already exist
-                           if (dataSnapshot.child(Phone.getText().toString()).exists()) {
-                               mDialog.dismiss();
-                               Toast.makeText(SignUp.this, "Phone Number Already Registered!!!", Toast.LENGTH_SHORT).show();
-
-                           } else {
-                               mDialog.dismiss();
-                               User user = new User(Name.getText().toString(), Password.getText().toString());
-                               table_user.child(Phone.getText().toString()).setValue(user);
-                               Toast.makeText(SignUp.this, "Sign Up successfully", Toast.LENGTH_SHORT).show();
-                               finish();
-                           }
-
-                       }
-
-                       @Override
-                       public void onCancelled(DatabaseError databaseError) {
-
-                       }
-                   });
-               }
-               else
-               {
-                   Toast.makeText(SignUp.this, "Please Check Your Internet Connection !", Toast.LENGTH_SHORT).show();
-                   return;
-               }
-            }
-        });
 
     }
 }
