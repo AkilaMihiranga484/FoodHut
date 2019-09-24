80034ab935e3fac96622a93670e9b0995fb05537
AkilaUdanaUDS <akila.uds@gmail.com>
2019-09-24 14:46:06 +0530

    order management full code

--- app/src/main/java/com/example/foodhut/HandleOrderStatus.java
+++ app/src/main/java/com/example/foodhut/HandleOrderStatus.java
@@ -23,13 +23,106 @@ import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class HandleOrderStatus extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
 
+    FirebaseRecyclerAdapter<Request, HandleOrderViewHolder> adapter;
+
+    FirebaseDatabase db;
+    DatabaseReference requests;
+
+    MaterialSpinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handle_order_status);
+
+        //Firebase
+        db = FirebaseDatabase.getInstance();
+        requests = db.getReference("Requests");
+
+        //Init
+        recyclerView = (RecyclerView)findViewById(R.id.listHandleOrders);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        LoadOrders();
                 
     }
 
+    private void LoadOrders() {
+        adapter = new FirebaseRecyclerAdapter<Request, HandleOrderViewHolder>(
+                Request.class,
+                R.layout.handle_order_layout,
+                HandleOrderViewHolder.class,
+                requests
+
+        ) {
+            @Override
+            protected void populateViewHolder(HandleOrderViewHolder handleOrderViewHolder, Request request, int i) {
+                handleOrderViewHolder.txtOrderId.setText(adapter.getRef(i).getKey());
+                handleOrderViewHolder.txtOrderStatus.setText(Common.convertCodeToStatus(request.getStatus()));
+                handleOrderViewHolder.txtOrderPhone.setText(request.getPhone());
+                handleOrderViewHolder.txtOrderAddress.setText(request.getAddress());
+
+                handleOrderViewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+
+                    }
+                });
+            }
+        };
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        if(item.getTitle().equals(Common.UPDATE))
+            showUpdateDialog(adapter.getRef(item.getOrder()).getKey(),adapter.getItem(item.getOrder()));
+        else if(item.getTitle().equals(Common.DELETE))
+            deleteOrder(adapter.getRef(item.getOrder()).getKey());
+
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteOrder(String key) {
+        requests.child(key).removeValue();
+    }
+
+    private void showUpdateDialog(String key, final Request item) {
+        final AlertDialog.Builder alertDialog = new AlertDialog.Builder(HandleOrderStatus.this);
+        alertDialog.setTitle("Update Order");
+        alertDialog.setMessage("Please Choose Status");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View view = inflater.inflate(R.layout.update_order_layout,null);
+
+        spinner = view.findViewById(R.id.statusSpinner);
+        spinner.setItems("Placed","On My Way","Shipped");
+
+        alertDialog.setView(view);
+
+        final String lacalKey = key;
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                item.setStatus(String.valueOf(spinner.getSelectedIndex()));
+
+                requests.child(lacalKey).setValue(item);
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
 }
